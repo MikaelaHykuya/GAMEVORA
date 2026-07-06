337df60017be214471827d6b21c57da8068a637d
MikaelaHykuya <mikaela@example.com>
2026-07-06 13:28:12 +0700

    fix: base path '/' untuk custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/GAMEVORA/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
