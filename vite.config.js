fac84b71b26efe68c87c721472605647c8b48122
MikaelaHykuya <MikaelaHykuya@users.noreply.github.com>
2026-07-29 00:28:56 +0700

    Fix SRI plugin import syntax

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-import { sri } from 'vite-plugin-sri'
+import sri from 'vite-plugin-sri'
 
 export default defineConfig({
   base: '/',
